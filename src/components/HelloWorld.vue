e2958d2e234abb0d9431abb7a8092a0f83aec439
xuyujie <1125397421qq.com>
2025-02-27 15:22:30 +0800

    组件互传

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,7 @@
   <button v-on:click="counter+=1">{{counter}}</button>
   <button @click="clickButton2">{{ message }}</button>
 
+
   <!-- 实现 ：获取列表人员姓名，并且为每一个人员姓名添加点击时间，点击后显示人员姓名到控制台-->
    <ul>
     <li @click="clickButton(item)" v-for="(item,index) in items" :key="index"> {{ item }} </li>
@@ -15,7 +16,9 @@
    <input type="text" v-model.lazy.trim="username">
    <p>{{ username }}</p>
 
-   <MyCom/>
+   <!-- <MyCom :message="message" :names="items"/> -->
+   <MyCom @onEvent="clickThrough"/>
+   
 </template>
 
 <script>
@@ -40,7 +43,16 @@ export default {
     clickButton2(event){
       // this.message="点击了"
       event.target.innerHTML='ddd'
+    },
+    clickThrough(data){
+      this.mm=data;
+      console.log(data);
     }
+      
+    
+  },
+  components:{
+    MyCom
   }
 }
 </script>
--- src/components/myCom.vue
+++ src/components/myCom.vue
@@ -1,14 +1,42 @@
 <template>
-    <h3>hiiiiiii</h3>
+    <h3>传递数据</h3>
+    <h3>{{ message }}</h3>
+    <ul>
+        <li v-for="(item,index) in names" key="index">{{ item }}</li>
+    </ul>
+    <button @click="clickThrough">点击</button>
 </template>
 
 <script>
 export default{
-    name:"myCom"
+    name:"myCom",
+    // props:{
+    //     message:{
+    //         type:String,
+    //         default:""
+    //     },
+    //     names:{
+    //         type:Array,
+    //         default:function(){
+    //             return []
+    //         }
+    //     }
+    // },
+    methods:{
+        clickThrough(){
+            this.$emit("onEvent",this.message)
+        }
+    },
+    data(){
+        return{
+            message:"我是component数据"
+        }
+    },
 }
+
 </script>
 
-<style>
+<style scoped>
 h3{
     color:red;
     font-size: 40px;
