aab2113eb224efdd40775e58460c467a9727a896
xuyujie <1125397421qq.com>
2025-02-27 10:48:02 +0800

    github

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,46 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h3>hi</h3>
+    <p>{{ message }}</p>
+    <p v-if="flag">true</p>
+    <p v-else>false</p>
   </div>
+  <button v-on:click="counter+=1">{{counter}}</button>
+  <button @click="clickButton2">{{ message }}</button>
+
+  <!-- 实现 ：获取列表人员姓名，并且为每一个人员姓名添加点击时间，点击后显示人员姓名到控制台-->
+   <ul>
+    <li @click="clickButton(item)" v-for="(item,index) in items" :key="index"> {{ item }} </li>
+   </ul>
+   <input type="text" v-model.lazy.trim="username">
+   <p>{{ username }}</p>
+
+   <MyCom/>
 </template>
 
 <script>
+import MyCom from './myCom.vue';
+
+//实现：获取列表人员姓名，并且为每一个人员姓名添加点击时间，点击后显示人员姓名到控制台
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      message:'ceshi',
+      flag:true,
+      counter:0,
+      items:["iwen","ime","kathy"],
+      username:""
+    }
+  },
+  methods:{
+    clickButton(item){
+      console.log(item);
+    },
+    clickButton2(event){
+      // this.message="点击了"
+      event.target.innerHTML='ddd'
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/myCom.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3>hiiiiiii</h3>
+</template>
+
+<script>
+export default{
+    name:"myCom"
+}
+</script>
+
+<style>
+h3{
+    color:red;
+    font-size: 30px;
+}
+</style>
